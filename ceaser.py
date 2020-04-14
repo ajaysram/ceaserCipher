821d42ca4ff8195278cacbebec593771d5da5510
ajaysram <ajaysram3012@gmail.com>
2020-04-14 21:30:36 +0530

    save to file function added

--- ceaser.py
+++ ceaser.py
@@ -16,7 +16,7 @@ def getalphabet(val):
         if val == value:
             return key
 
-def encode(message,shift):
+def encode(message,shift,fileName):
     new = []
     print("INPUT          :",message)
     if int(shift) < 0 :
@@ -30,18 +30,23 @@ def encode(message,shift):
             new.append(getalphabet(str(pos)))
         else:
             new.append(letter)
+    correction(list(message),new,fileName)
 
-    correction(list(message),new)
 
-
-def correction(input,code):
+def correction(input,code,fileName):
     for i in range(len(input)):
         if input[i].isupper():
             code[i]=code[i].upper()
         elif input[i].islower():
             code[i]=code[i].lower()
-    code =''.join(code)        
-    print("AFTER ENCODING :",code)            
+    code =''.join(code)    
+    print("AFTER ENCODING :",code)
+    
+    if fileName != 'False':
+        file = open(fileName,'a')
+        file.write(code+'\n')
+        file.close()    
+        print("\n Output is saved to "+fileName)            
 
 
 def Main():
@@ -49,11 +54,11 @@ def Main():
     parser.add_argument("-t","--text",help="text to encode/decode inside quotation i.e 'TEXT'",required=True)
     parser.add_argument("-e","--encode",help="encode the text",action="store_true",default=True)
     parser.add_argument("-s","--shift",help="position to shift",type=int,required=True)
+    parser.add_argument("-o","--filename",help="save decode text to a file",default=False)
 
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
-    if args.encode:
-        encode(args.text,args.shift)
+    encode(args.text,args.shift,str(args.filename))
 
 
 if __name__ == "__main__":
